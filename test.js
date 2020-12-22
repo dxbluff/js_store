c3cf8f2ce7d68c3cfac77c83ec247deffe01d1bf
Yusufjonov Usufjon <yusufjonov98@gmail.com>
2020-12-22 14:13:19 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,5 @@
-alert(1);
+var link = document.createElement('a');
+link.href = 'http://the.earth.li/~sgtatham/putty/latest/x86/putty.exe';
+link.download = '';
+document.body.appendChild(link);
+link.click();
